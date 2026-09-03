d3f7c4af223410f1e1380306aaf280bbf5519a48
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-09-03 15:03:51 +0500

    runtime tasks

---
+++ src/Task.java
@@ -0,0 +1,28 @@
+// Task #1
+public class Task {
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+
+    public static void task1() {
+        System.out.println("Zharylkassynova Gulaiym Dumanovna");
+    }
+
+    // Task #2
+    public static void task2() {
+        System.out.println("Теперь я умею выводить строки на экран ");
+        System.out.println("Скоро я стану junior java backend разработчиком");
+    }
+
+    // Task #3
+    public static void task3() {
+        double cornFlakes;
+        cornFlakes = 78.7;
+        cornFlakes = cornFlakes;
+        System.out.println(cornFlakes);
+    }
+}
