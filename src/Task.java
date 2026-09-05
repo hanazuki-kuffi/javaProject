8ddf390e17340cd964d9216e103f222acfbaf147
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-09-05 22:36:07 +0500

    runtime tasks

--- src/Task.java
+++ src/Task.java
@@ -5,6 +5,11 @@ public class Task {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
 
 
@@ -25,4 +30,59 @@ public class Task {
         cornFlakes = cornFlakes;
         System.out.println(10 + 12 + 10);
     }
+
+    public static void task4() {
+        double megabytes = 32409.6;
+        double megabytesPerGigabyte = 1024;
+        double gigabytes = megabytes / megabytesPerGigabyte;
+
+            System.out.println(gigabytes);
+
+    }
+
+    public static void task5() {
+
+        int foodPrice = 8 * 5;
+        int bagPrice = 4 * 5 + 4 * 3;
+        int totalSum = foodPrice + bagPrice;
+
+        System.out.println(totalSum);
+
+    }
+
+    public static void task6() {
+
+        String hello = "Привет!";
+        String name = "Kuffi";// Объявите переменную и присвойте ей значение - ваше имя
+        System.out.println(hello);
+        System.out.println(name); // Здесь нужно напечатать имя
+    }
+
+    public static void task7() {
+
+        String hello = "Привет!";
+        String name = "Kuffi"; // Замените на ваше имя
+
+        System.out.println(hello + name + ".");// Напечатайте приветствие
+
+    }
+
+    public static void task8() {
+        String hello = "Привет!";
+        String myNameIsPixel = "Меня зовут Пиксель.";
+        String iAmCat = "Я кот.";
+
+        System.out.println(hello + " " + myNameIsPixel + " " + iAmCat);// Напечатайте фразу
+
+    }
+
+
+
+
+    public static void task9() {
+            double tenges = 35000; // Напечатайте значение
+
+    }
 }
+
+
