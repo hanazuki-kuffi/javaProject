b4ac2a192139cb0eaff31c7d9d69208878d9887c
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-09-07 20:01:43 +0500

    runtime tasks

--- src/Practice.java
+++ src/Practice.java
@@ -2,6 +2,7 @@ public class Practice {
     public static void main(String[] args) {
         practice1();
         practice2();
+        practice3();
         practice4();
     }
 
@@ -25,16 +26,25 @@ public class Practice {
             System.out.println(isMbMore);
         }
 
-//        public static void practice3() {
-//
-//        int answer1 = 3;
-//        int answer2 = 2;
-//        int answer3 = 1;
-//
-//            System.out.println("Результат проверки вопроса 1:");
-//
-//
-//        }
+
+        public static void practice3() {
+
+        int answer1 = 3; // Ответ на 1 вопрос      ПРАВИЛЬНЫЙ ВАРИАНТ: 2
+        int answer2 = 2; // Ответ на 2 вопрос      ПРАВИЛЬНЫЙ ВАРИАНТ: любой кроме 4
+        int answer3 = 1; // Ответ на 3 вопрос      ПРАВИЛЬНЫЙ ВАРИАНТ: 1
+
+            boolean answerNumberOne = answer1 == 2;
+            boolean answerNumberTwo = answer2 != 4;
+            boolean answerNumberThree = answer3 == 1;
+
+            System.out.println("Результат проверки вопроса 1: " + answerNumberOne);
+            System.out.println("Результат проверки вопроса 2: " + answerNumberTwo);
+            System.out.println("Результат проверки вопроса 3: " + answerNumberThree);
+
+
+
+
+        }
 
     public static void practice4() {
         String pixelCat = "Пиксель — хороший кот!";
