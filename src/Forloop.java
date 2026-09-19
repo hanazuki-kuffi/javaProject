b3bbd551959c8d5175a642ab3b1bffd9571bf3fe
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-09-19 22:42:39 +0500

    my runtime tasks

--- src/Forloop.java
+++ src/Forloop.java
@@ -1,17 +1,17 @@
 public class Forloop {
     public static void main(String[] args) {
-//        loop1();
+        loop1();
 //        loop2();
 //        loop3();
-        loop4();
+//        loop4();
     }
 
     public static void loop1() {
-        int money = 2500; // начальная общая сумма на корм
+        int moneyStart = 2500; // начальная общая сумма на корм
         int foodPerDay = 500; // цена для одного корма на 1 день
         int days = 0;
 
-        for (money = 2000; money >= 0; money -= foodPerDay) {
+        for (int money = moneyStart - foodPerDay; money >= 0; money -= foodPerDay) {
             days += 3;
             System.out.println("На " + days + "-й день останется " + money + "тенге.");
         }
@@ -20,32 +20,31 @@ public class Forloop {
 
     public static void loop2() {
 
+        int startEpisode = 1; // как  я могу менять значение с помощью переменные счетчиками
+        int endEpidose = 3; // как  я могу менять значение с помощью переменные счетчиками
+
         for (int day = 1; day <= 30; day++) {
             System.out.println("День " + day);
 
-            int startEpisode = 1; // как  я могу менять значение с помощью переменные счетчиками
-            int endEpidose = 3; // как  я могу менять значение с помощью переменные счетчиками
-
             for (int j = startEpisode; j <= endEpidose; j++) {
                 System.out.println("Серия " + j);
-
-                startEpisode += 3;
-                endEpidose += 3;
-
             }
+
+            startEpisode += 3;
+            endEpidose += 3;
         }
         System.out.println("Ура! Вы прокачали и Java, и английский!");
     }
+
+
     public static void loop3() {
         int everyFlatsNumber = 5; // Количество квартир на этаже
         int floorsNumber = 10;// Количество этажей
 
         for (int i = 1; i <= floorsNumber; i++) {
-            for (int j = 1; j <= everyFlatsNumber; j++) {
-
-//                int currentFlat = (... - 1) * ... + ...;
-
-                System.out.println("Этаж" + i + " квартира " + j + " — доставлено.");
+            int currentFlat = (i - 1) * everyFlatsNumber + 1;
+            for (int j = currentFlat; j < currentFlat + everyFlatsNumber; j++) {
+                System.out.println("Этаж " + i + " квартира " + j + " — доставлено.");
             }
         }
     }
@@ -55,13 +54,10 @@ public class Forloop {
 
             System.out.println("Круг " + i);
 
-            if (i % 2 == 0) {// Выполнение каждого круга зависит от условия - проверьте, является ли круг чётным
-                for (int a = 1; a <= 5; a++)
-                System.out.println("  Приседаем " + a);
-            } else {
-                for (int j = 1; j <= 3; j++) {
-                    System.out.println("  Отжимаемся " + j);
-                }
+            if (i % 2 == 0) // Выполнение каждого круга зависит от условия - проверьте, является ли круг чётным
+                for (int a = 1; a <= 5; a++) System.out.println("  Приседаем " + a);
+             else {
+                for (int j = 1; j <= 3; j++) System.out.println("  Отжимаемся " + j);
             }
             for (int z = 1; z <= 7; z++) {
                 System.out.println("  Качаем пресс " + z);
--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,18 +1,49 @@
+import java.util.Random;
 import java.util.Scanner;
+
 
 public class WhileLoop {
 
     public static void main(String[] args) {
         whileloop1();
     }
+
     public static void whileloop1() {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Сколько денег у вас сейчас: ");
         double balance = scanner.nextDouble();
 
+        double percent = 0.05;
+
         int years = 0;
 
+        while (balance <= 1_000_000) {
+            balance += balance * percent;
+            years++;
+        }
 
+        System.out.println("Через " + years + " лет у вас будет миллион!");
+
+    }
+
+    public static void whileloop2() {
+
+        Scanner scanner = new Scanner(System.in);
+        int randomInt = new Random().nextInt(1000); // Генерирует новое число от 0 до 1000
+        int userInput = -1; // Это нужно, чтобы цикл запустился, если Random выдаст 0
+        System.out.println("Я загадал число от 0 до 1000.");
+
+        while (userInput <= randomInt) {
+            System.out.println("Ваш ход:"); // Напишите условие цикла для запуска игры
+            userInput = scanner.nextInt(); // В этой переменной должен сохраняться ввод пользователя
+            if (userInput < randomInt) { // Условие проверяется в цикле
+                System.out.println("Меньше");
+            } else {
+                System.out.println("Больше");
+            }
+            // Печатаем, когда число угадано
+            System.out.println("Вы великолепны! Именно это я загадал.");
+        }
     }
 }
